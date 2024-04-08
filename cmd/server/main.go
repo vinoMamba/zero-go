8be230670fa134d2607df2446c50d2c6995db9a6
vino <vino0908@outlook.com>
2024-04-08 22:52:58 +0800

    feat: 初始化日志

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -4,11 +4,16 @@ import (
 	"fmt"
 
 	"github.com/vinoMamba/zero/pkg/config"
+	"github.com/vinoMamba/zero/pkg/log"
+	"go.uber.org/zap"
 )
 
 func main() {
 	// init config
 	conf := config.NewConfig()
+	// init logger
+	logger := log.NewLog(conf)
 
-	fmt.Println(conf.GetString("env"))
+	logger.Info("server start", zap.String("host", fmt.Sprintf("http://%s:%d", conf.GetString("http.host"), conf.GetInt("http.port"))))
+
 }
